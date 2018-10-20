f41f83d062102af3418a5fc616dbcff02fd1162a
ferburs <you@example.com>
2018-10-20 19:11:36 -0300

    aca

--- 05-hash/hash.c
+++ 05-hash/hash.c
@@ -1,6 +1,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "hash.h"
+#define LARGO_INICIAL 10
+
+/* *****************************************************************
+ *                             STRUCTS
+ * *****************************************************************/
+
+
+typedef struct estado{
+    char* vacio;
+    char* ocupado;
+    char* borrado;
+
+}estado_t;
 
 typedef struct nodo {
 
@@ -9,12 +22,45 @@ typedef struct nodo {
 
 }nodo_t;
 
-struct hash{
+typedef struct hash_campo {
+    char *clave;
+    void *valor;
+    estado_t estado; 
+} hash_campo_t;
 
+
+
+struct hash {
+    size_t cantidad;                  
+    size_t largo;                     
+    size_t carga;                     
+    hash_campo_t *tabla; 
+    hash_destruir_dato_t destruir_dato; 
 };
+
 struct hash_iter{
 
 };
+
+/* *****************************************************************
+ *                              EXTRAS
+ * *****************************************************************/
+
+hash_campo_t* crear_tabla_hash (){
+
+    hash_campo_t* tabla = malloc((hash->largo)*sizeof(hash_campo_t*));
+
+    if (!tabla) return NULL;
+
+    for (int i=0;i<hash->largo;i++)
+
+      tabla[i]=VACIO;           //EL vacio esta mal 
+
+    tabla->clave = NULL;     //         no tengo ni 
+    tabla->valor = NULL;     //         idea de como
+    tabla->estado = "vacio"; //         van estos tres
+    return tabla;
+}
 
 nodo_t *crear_nodo(void *dato){
 
@@ -28,13 +74,58 @@ nodo_t *crear_nodo(void *dato){
     return nodo;
 }
 
-hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
+   
+int Hashing(char *cad){	
+    int valor;
+    for (valor=0; ; c++)
+   	  valor += (int)*c;
+    return (valor%hash->largo);
+}
 
+
+/* *****************************************************************
+ *                            PRIMITIVAS
+ * *****************************************************************/
+
+
+hash_t *hash_crear(hash_destruir_dato_t destruir_dato){
     hash_t* hash = malloc(sizeof(hash_t));
 
     if(!hash)  return NULL;
-    //aca va todo lo que tenga en el struct pero ni idea que tiene
+
+    hash->tabla = crear_tabla_hash();
+
+    if(!tabla){
+
+        free(hash);
+
+        return NULL;
+    }
+    hash->destruir_dato = destruir_dato;
+
+    hash->largo = LARGO_INICIAL;
+
+    hash->cantidad = 0;
+
+    hash->carga = 0;
 
     return hash;
 }
 
+size_t hash_cantidad(const hash_t *hash){
+    return hash->cantidad;
+}
+
+bool hash_guardar(hash_t *hash, const char *clave, void *dato){
+    nodo_t* nodo = crear_nodo(dato);
+    if (nodo == NULL){
+        return false;
+    }
+}
+
+void hash_destruir(hash_t *hash){
+    for (int t = 0 , i<hash->largo, i++){
+        hash->destruir_dato(hash->tabla[i]);
+    }
+    free(hash)
+}
